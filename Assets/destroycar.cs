252ceaa2e02f4d4e97181d1eff5d2f97315af84c
poomwarning <thanaphum.14@mail.kmutt.ac.th>
2020-04-09 04:09:15 +0700

    nothing change

---
+++ Assets/destroycar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class destroycar : MonoBehaviour
+{
+   public GameObject car;
+    
+
+   private void ontriggerEnter(Collider other) 
+   {
+         if(other.tag=="car")
+         {
+             Destroy(car);
+         }
+    }
+
+}
---
+++ Assets/poop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class poop : MonoBehaviour
+{
+    public GameObject UIobject;
+    public GameObject Box;
+    
+    public GameObject car;
+    
+    
+     private void Update() {
+       car.transform.position +=  Vector3.forward * (Time.deltaTime*2);
+    }
+    void Start()
+    {
+        UIobject.SetActive(false);
+    }
+     private void OnTriggerEnter(Collider other) {
+        if(other.tag=="Player")
+        {
+            UIobject.SetActive(true);
+            
+        }
+      
+    }
+
+
+     private void OnTriggerExit(Collider other) 
+     {
+        UIobject.SetActive(false);
+    }
+}
