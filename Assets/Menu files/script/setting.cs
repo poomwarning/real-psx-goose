8a5c2ceef294cee9660a52d4a1d1f67beb919fb1
Jack_Lewise <wai.test@mail.kmutt.ac.th>
2020-04-20 22:54:14 +0700

    menu_progress

---
+++ Assets/Menu files/script/setting.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setting : MonoBehaviour
+{
+    public void optionvolume (float volume)
+    {
+        Debug.Log(volume);
+    }
+}
