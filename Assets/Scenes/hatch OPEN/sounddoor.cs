84bf111606becd23b3cf289f128cae8c45731edc
poomwarning <thanaphum.14@mail.kmutt.ac.th>
2020-05-04 01:19:40 +0700

    update sewer map

---
+++ Assets/Scenes/hatch OPEN/sounddoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sounddoor : MonoBehaviour
+{
+   public AudioSource openhatch;
+   public GameObject hatch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TheSequence());
+    }
+
+    IEnumerator TheSequence()
+    {
+        yield return new WaitForSeconds(4.0f);
+        openhatch.Play();
+        hatch.GetComponent<Animation>().Play("hatchopen");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
