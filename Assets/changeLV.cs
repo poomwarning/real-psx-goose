4528e89a33be92e28390b6f6164cd0477c3a88e9
poomwarning <thanaphum.14@mail.kmutt.ac.th>
2020-05-08 20:26:33 +0700

    update

---
+++ Assets/changeLV.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+
+public class changeLV : MonoBehaviour
+{
+   
+    public string levelToLoad;
+    // Use this for initialization
+    public GameObject fade;
+
+    void Start()
+    {
+      
+    }
+    void Update()
+    {
+     
+
+    }
+
+    // Update is called once per frame
+    void  OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerL")
+        {
+         
+            if ( Input.GetButtonDown("Use"))
+            {
+                StartCoroutine(oof());
+                
+                //Application.LoadLevel(levelToLoad);
+            }
+        }
+    }
+    IEnumerator oof()
+    {
+        fade.GetComponent<Animation>().Play("fadeINsewer");
+        yield return new WaitForSeconds(3.5f);
+        Application.LoadLevel(levelToLoad);
+        
+    }
+
+   
+}
---
+++ Assets/mouselook.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouselook : MonoBehaviour
+{
+    private float sensitivity = 50f;
+
+    private float sensMultiplier = 1f;
+    
+
+    public Transform playerCam;
+
+    public Transform orientation;
+
+    private float xRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+     private void Update() {
+      
+        Look();
+    }
+
+    // Update is called once per frame
+  private float desiredX;
+    private void Look() {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+
+        //Find current look rotation
+        Vector3 rot = playerCam.transform.localRotation.eulerAngles;
+        desiredX = rot.y + mouseX;
+        
+        //Rotate, and also make sure we dont over- or under-rotate.
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //Perform the rotations
+        playerCam.transform.localRotation = Quaternion.Euler(xRotation, desiredX, 0);
+        orientation.transform.localRotation = Quaternion.Euler(0, desiredX, 0);
+    }
+
+}
