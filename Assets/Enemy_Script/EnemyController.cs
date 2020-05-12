e58b4455a61f1dbcd589b4bad54d7bf4a241cb24
Delayman <61342646+Delayman@users.noreply.github.com>
2020-05-12 15:33:06 +0700

    Enemy Ai Pathfinding #1
    Pathfinding frist ver.

---
+++ Assets/Enemy_Script/EnemyController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    [SerializeField]
+    Transform traget;
+    [SerializeField]
+    Vector3 Destination;
+    NavMeshAgent agent;
+    [SerializeField]
+    bool isFollowing;
+    [SerializeField]
+    private Level_Ctrl L_Ctrl;
+    
+    void Start()
+    {       
+        agent = GetComponent<NavMeshAgent>();
+        Destination = agent.destination;
+    }
+
+    
+    void Update()
+    {
+        
+        if(Vector3.Distance(Destination,traget.position) < lookRadius)
+        {
+            Destination = traget.position ;
+            agent.SetDestination(Destination);
+
+            agent.speed = 18f;
+
+        }
+    }
+
+    void OnDrawGizmos() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+
+    }
+
+    void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.tag == ("Player"))
+        {
+            L_Ctrl.Scene_Changer();
+
+        }
+    }
+}
---
+++ Assets/Enemy_Script/Level_Ctrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Level_Ctrl : MonoBehaviour
+{
+    public void Scene_Changer()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene("Jumpscare_tem");
+    }
+
+    public void Scene_chager2()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Scene_chager3()
+    {
+        SceneManager.LoadScene("Next_Level");
+    }
+    
+}
