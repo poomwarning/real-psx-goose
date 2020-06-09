928ef7b6b79e24dd5a1f6ab8e2e23e2895f56d4e
poomwarning <thanaphum.14@mail.kmutt.ac.th>
2020-06-09 15:44:14 +0700

    Revert "Enemy_Chasing"
    This reverts commit 68eb71ff20a89c59625beac0528d9da046ce090c.

--- Assets/Enemy_Script/PlayerMovement_test.cs
+++ Assets/Enemy_Script/PlayerMovement_test.cs
@@ -9,7 +9,11 @@ public class PlayerMovement_test : MonoBehaviour
     public Level_Ctrl lv_ctrl;
     public float speed = 12f;
     [SerializeField]
-    
+    private GameObject key;
+    private bool IsDoorsucc = false;
+
+    private bool Locked_door = true;
+    private bool AreDoorSUCC = false;
 
     // Update is called once per frame
     void Update()
@@ -20,20 +24,65 @@ public class PlayerMovement_test : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(IsDoorsucc && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("yoink");
+            lv_ctrl.Scene_chager3();
+        }
+
+        if(AreDoorSUCC && Input.GetKeyDown(KeyCode.E) && Locked_door == false)
+        {
+            Debug.Log("yote");
+            lv_ctrl.Scene_chager3();
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Delete))
+        {
+            PlayerPrefs.DeleteKey("Key1");
+            Debug.Log("yeet");
+        }
     }
 
     void OnTriggerEnter(Collider other) 
     {
-        
+        if(other.gameObject.tag == "Door1")
+        {
+            IsDoorsucc = true;            
+        }
+
+        if(other.gameObject.tag == "Key")
+        {
+            PlayerPrefs.SetInt("Key1",1);
+            Locked_door = false;
+            Destroy(key); 
+            
+        }
+
+        if(other.gameObject.tag == "Door2")
+        {
+            AreDoorSUCC = true;
+        }
     }
 
     
     void OnTriggerExit(Collider other)
     {
-        
+        if(other.gameObject.tag == "Door1")
+        {
+            IsDoorsucc = false;            
+        }
+
+        if(other.gameObject.tag == "Door2")
+        {
+            AreDoorSUCC = false;
+        }
     }
     void Start() 
     {
-        
+        if(PlayerPrefs.GetInt("Key1") == 1)
+        {
+            Locked_door = false;
+        }
     }
 }
