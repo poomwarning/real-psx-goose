c2f40f4d14c48ddded8c60216f94d4f0f4dd34f0
poomwarning <thanaphum.14@mail.kmutt.ac.th>
2020-06-13 01:06:24 +0700

    table fall

---
+++ Assets/jumpscare.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpscare : MonoBehaviour
+{
+    public GameObject jumpscarepoop;
+    public AudioSource jumpscaresound;
+
+     private void Start() 
+    {
+        jumpscarepoop.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            StartCoroutine(jumpevent());
+        }
+    }
+    IEnumerator jumpevent()
+    {
+        jumpscaresound.Play();
+        jumpscarepoop.SetActive(true);
+         yield return new WaitForSeconds(0.3f);
+        jumpscarepoop.SetActive(false);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/tabletrig.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tabletrig : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator tablefall;
+    void Start()
+    {
+       // tablefall.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            tablefall.SetBool("doorfall",true);
+            Destroy(gameObject);
+        }
+    }
+    
+}
